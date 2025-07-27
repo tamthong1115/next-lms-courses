b742857fff47ee0f2c162bfa8ee54bd664244e97
Tam Thong <thong@192.168.1.234.non-exists.ptr.local>
2025-07-27 23:33:28 +0700

    add arcjet (secure app) to protect sign up form

---
+++ lib/arcject.ts
@@ -0,0 +1,31 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+} from "@arcjet/next";
+import { env } from "./env";
+
+// Re-export the rules to simplify imports inside handlers
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+};
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  // Use the fingerprint characteristic for
+  // identifying and tracking users of requests.
+  characteristics: ["fingerprint"],
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
