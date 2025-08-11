7afb6de4f125295cda548f9ba3b855b1826b3915
Tam Thong <TamThong@mail.com>
2025-08-12 01:10:36 +0700

    file deletion

---
+++ app/api/s3/delete/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import { DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { env } from '@/lib/env';
+import { S3 } from '@/lib/S3Client';
+
+export async function DELETE(request: Request) {
+  try {
+    const data = await request.json();
+
+    const { key } = data;
+
+    if (!key) {
+      return NextResponse.json(
+        { error: 'Missing or invalid key' },
+        { status: 400 }
+      );
+    }
+
+    const command = new DeleteObjectCommand({
+      Bucket: env.NEXT_PUBLIC_S3_BUCKET_NAME_IMAGES,
+      Key: key,
+    });
+
+    await S3.send(command);
+    return NextResponse.json(
+      { message: 'File deleted successfully' },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error deleting file:', error);
+    return NextResponse.json(
+      { error: 'Failed to delete file' },
+      { status: 500 }
+    );
+  }
+}
--- components/file-uploader/RenderState.tsx
+++ components/file-uploader/RenderState.tsx
@@ -1,12 +1,18 @@
-import { CloudUploadIcon, ImageIcon } from 'lucide-react';
+import { CloudUploadIcon, ImageIcon, XIcon } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { AppLoader } from '@/components/ui/loader';
 
 export function RenderEmptyState({ isDragActive }: { isDragActive: boolean }) {
   return (
     <div className="text-center">
       <div className="flex items-center mx-auto justify-center size-12 rounded-full bg-muted mb-4">
         <CloudUploadIcon
-          className={cn('size-6 text-muted-foreground', isDragActive && 'text-primary')}
+          className={cn(
+            'size-6 text-muted-foreground',
+            isDragActive && 'text-primary'
+          )}
         />
       </div>
 
@@ -16,7 +22,9 @@ export function RenderEmptyState({ isDragActive }: { isDragActive: boolean }) {
         ) : (
           <>
             Drag and drop files here, or{' '}
-            <span className="text-primary font-bold cursor-pointer">click to select files</span>
+            <span className="text-primary font-bold cursor-pointer">
+              click to select files
+            </span>
           </>
         )}
       </p>
@@ -33,7 +41,57 @@ export function RenderErrorState() {
 
       <p className="text-base font-semibold">Upload Failed</p>
       <p className="text-xs mt-1 text-muted-foreground">Something went wrong</p>
-      <p className="text-xl mt-3 text-muted-foreground">Click or drag file to retry</p>
+      <p className="text-xl mt-3 text-muted-foreground">
+        Click or drag file to retry
+      </p>
+    </div>
+  );
+}
+
+export function RenderUploadedState({
+  previewUrl,
+  isDeleting,
+  handleRemoveFile,
+}: {
+  previewUrl: string;
+  isDeleting: boolean;
+  handleRemoveFile: () => void;
+}) {
+  return (
+    <div>
+      <Image
+        src={previewUrl}
+        alt="Uploaded File Preview "
+        fill
+        className="object-contain p-2"
+      />
+      <Button
+        variant="destructive"
+        size="icon"
+        className={cn('absolute top-4 right-4')}
+        onClick={handleRemoveFile}
+        disabled={isDeleting}
+      >
+        {isDeleting ? <AppLoader /> : <XIcon className="size-4" />}
+      </Button>
+    </div>
+  );
+}
+
+export function RenderUploadingState({
+  progress,
+  file,
+}: {
+  progress: number;
+  file: File;
+}) {
+  return (
+    <div className="text-center flex justify-center items-center flex-col">
+      <p>{progress}</p>
+      <p className="mt-2 text-sm font-medium text-foreground">Uploading...</p>
+      <p className="mt-1 text-xs text-muted-foreground truncate max-w-xs">
+        {file.name}
+      </p>
     </div>
   );
 }
